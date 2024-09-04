719429ce11e956a1fef8a1b5f278be28ee18a3f9
King Villanueva <king.villanueva@ballys.com>
2024-09-04 08:30:14 +0800

    [fix] Setup Controller routes

--- public/index.php
+++ public/index.php
@@ -5,6 +5,15 @@ use Salad\Core\Application;
 use Salad\Core\Router;
 use Salad\Controllers\HomeController;
 use Symfony\Component\Yaml\Yaml;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+if ($_ENV['IS_SET']!== 'true') {
+  // Redirect to the setup page
+  header('Location: /setup.php');
+  exit;
+}
 
 $application = new Application(dirname(__DIR__));
 
